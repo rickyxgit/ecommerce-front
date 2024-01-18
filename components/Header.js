ce4e330682d60e1b456852c6ff676c2efe4cbd6e
Ricky Xian <84099067+rickyxgit@users.noreply.github.com>
2024-01-18 12:05:13 -0800

    initial commit

---
+++ components/Header.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  background-color: #222; 
+ 
+`;
+
+
+export default function Header() {
+    return (
+        <StyledHeader>
+            <Link href={'/'}> Ecommerce</Link>
+            <nav>
+                <Link href={'/'}>Home</Link>
+                <Link href={'/products'}>All products</Link>
+                <Link href={'/categories'}>Categories</Link>
+                <Link href={'/account'}>Account</Link>
+                <Link href={'/cart'}>Cart (0)</Link>
+                
+            </nav>
+        </StyledHeader>
+    );
+}
