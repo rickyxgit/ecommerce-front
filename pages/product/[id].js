19b18150dc4bbfac035a45c0c5020adf2ae31a8f
Ricky Xian <84099067+rickyxgit@users.noreply.github.com>
2024-02-05 13:20:37 -0800

    ProductImages.js

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,28 +1,46 @@
 import Center from "@/components/Center";
 import Header from "@/components/Header";
 import Title from "@/components/Title";
+import WhiteBox from "@/components/WhiteBox";
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
+import styled from "styled-components";
 
-export default function ProductPage({product}) {
-    return (
-        <>
-        <Header />
-        <Center>
-            <Title></Title>
-        </Center>
-        </>
-    )
+const ColWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 0.8fr 1.2fr;
+  gap: 40px;
+  margin-top: 40px;
+`;
+
+export default function ProductPage({ product }) {
+  return (
+    <>
+      <Header />
+      <Center>
+        <ColWrapper>
+          <WhiteBox>
+
+            <img style={{maxWidth:'100%'}} src={product.images?.[0]} />
+          </WhiteBox>
+          <div>
+            <Title>{product.title}</Title>
+            <p>{product.description}</p>
+          </div>
+        </ColWrapper>
+      </Center>
+    </>
+  );
 }
 
 export async function getServerSideProps(context) {
-    await mongooseConnect();
-    const {id} = context.query;
-    const product = await Product.findById(id);
+  await mongooseConnect();
+  const { id } = context.query;
+  const product = await Product.findById(id);
 
-    return {
-        props: {
-            product: JSON.parse(JSON.stringify(product)),
-        }
-    }
+  return {
+    props: {
+      product: JSON.parse(JSON.stringify(product)),
+    },
+  };
 }
